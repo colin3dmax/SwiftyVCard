5c38163ab1ab9689798d659c1ca770bdd8f8b586
Ruslan Bolataev <krasnodar95@gmail.com>
2015-10-29 12:55:19 +0300

    Initial Commit

---
+++ SwiftyVCard/SwiftyVCard.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyVCard.h
+//  SwiftyVCard
+//
+//  Created by Ruslan on 29.10.15.
+//  Copyright © 2015 GRG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyVCard.
+FOUNDATION_EXPORT double SwiftyVCardVersionNumber;
+
+//! Project version string for SwiftyVCard.
+FOUNDATION_EXPORT const unsigned char SwiftyVCardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyVCard/PublicHeader.h>
+
+
